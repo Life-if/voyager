549578fee6f78eba2a86eeb874dcb1b6a4e725e9
Voyager <1241512309@qq.com>
2023-11-25 00:02:00 +0800

    0.0.0

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,24 @@
+import { getDirname, path } from '@vuepress/utils'
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url)
+
+export default defineUserConfig({
+  base: "/voyager/",
+
+  lang: "zh-CN",
+  title: "博客演示",
+  description: "vuepress-theme-hope 的博客演示",
+
+  theme,
+
+  alias:{
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",)
+  },
+  
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
